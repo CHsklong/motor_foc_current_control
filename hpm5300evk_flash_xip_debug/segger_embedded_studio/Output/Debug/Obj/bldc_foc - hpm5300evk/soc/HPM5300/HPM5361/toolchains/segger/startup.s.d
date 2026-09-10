7b8538857994b136573323e05944f9db0ffa2b56
unknown <17366636751@163.com>
2026-09-10 08:35:20 +0800

    忽略编译输出文件

--- hpm5300evk_flash_xip_debug/segger_embedded_studio/Output/Debug/Obj/bldc_foc - hpm5300evk/soc/HPM5300/HPM5361/toolchains/segger/startup.s.d
+++
@@ -1,4 +0,0 @@
-Output/Debug/Obj/bldc_foc\ -\ hpm5300evk/soc/HPM5300/HPM5361/toolchains/segger/startup.s.o: \
-  D:\sdk_env_v1.12.1\hpm_sdk\soc\HPM5300\HPM5361\toolchains\segger\startup.s \
-  ..\..\..\..\..\soc\HPM5300\HPM5361\hpm_csr_regs.h \
-  D:\sdk_env_v1.12.1\hpm_sdk\soc\HPM5300\HPM5361\toolchains\segger\..\vectors.h
